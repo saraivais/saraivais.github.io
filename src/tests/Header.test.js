620d0f0e108aa0c7a8c687c04dc392e4c3b24706
saraivais <isadorasaraiva95@gmail.com>
2022-07-17 15:52:03 -0300

    test [Header]: tests page navigation through header links

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,20 +1,20 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import Header from '../components/Header';
 import Home from '../routes/Home';
 import Projects from '../routes/Projects';
 import Contact from '../routes/Contact';
+import App from '../App';
 
 describe('Verifies the correct rendering of the Header component', () => {
   it('verifies the presence of the expected links in the Header component', () => {
-    const { debug } = render(
+    render(
       <MemoryRouter>
         <Header />
       </MemoryRouter>,
     );
-    debug();
 
     const headerNavigationLinks = screen.getAllByRole('link');
 
@@ -27,36 +27,78 @@ describe('Verifies the correct rendering of the Header component', () => {
   });
 
   it('verifies if the header component is rendered correctly in the "Home" page', () => {
-    const { debug } = render(
+    render(
       <MemoryRouter>
         <Home />
       </MemoryRouter>,
     );
-    debug();
 
     const homeLink = screen.getByRole('link', { name: /home/i });
     expect(homeLink).toBeInTheDocument();
   });
 
   it('verifies if the header component is rendered correctly in the "Contact" page', () => {
-    const { debug } = render(
+    render(
       <MemoryRouter>
         <Contact />
       </MemoryRouter>,
     );
-    debug();
     const contactLink = screen.getByRole('link', { name: /contact/i });
     expect(contactLink).toBeInTheDocument();
   });
 
   it('verifies if the header component is rendered correctly in the "Projects" page', () => {
-    const { debug } = render(
+    render(
       <MemoryRouter>
         <Projects />
       </MemoryRouter>,
     );
-    debug();
     const projectsLink = screen.getByRole('link', { name: /projects/i });
     expect(projectsLink).toBeInTheDocument();
+  });
+
+  it('verifies if the "Projects" link redirects to "Projects" page', () => {
+    const { debug } = render(
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>,
+    );
+    debug();
+
+    const projectsLink = screen.getByRole('link', { name: /projects/i });
+    userEvent.click(projectsLink);
+
+    const projectsPageTitle = screen.getByText(/projects screen/i);
+    expect(projectsPageTitle).toBeInTheDocument();
+  });
+
+  it('verifies if the "Contact" link redirects to "Contact" page', () => {
+    const { debug } = render(
+      <MemoryRouter initialEntries={['/projects']}>
+        <App />
+      </MemoryRouter>,
+    );
+    debug();
+
+    const contactMeLink = screen.getByRole('link', { name: /contact me/i });
+    userEvent.click(contactMeLink);
+
+    const contactPageTitle = screen.getByText(/contact screen/i);
+    expect(contactPageTitle).toBeInTheDocument();
+  });
+
+  it('verifies if the "Contact" link redirects to "Contact" page', () => {
+    const { debug } = render(
+      <MemoryRouter initialEntries={['/contact']}>
+        <App />
+      </MemoryRouter>,
+    );
+    debug();
+
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    userEvent.click(homeLink);
+
+    const homePageTitle = screen.getByText(/home screen/i);
+    expect(homePageTitle).toBeInTheDocument();
   });
 });
