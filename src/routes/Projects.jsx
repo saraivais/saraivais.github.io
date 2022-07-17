caff86baf1f02524b38bba19344a7072dfc48b65
saraivais <isadorasaraiva95@gmail.com>
2022-07-17 15:53:18 -0300

    feat [Projects]: adds Header component

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 function Projects() {
-  return (<div>Projects screen</div>);
+  return (
+    <div>
+      <Header />
+      Projects screen
+    </div>
+  );
 }
 
 export default Projects;
