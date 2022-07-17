9fb140e763f30b70f1ce223f599ee0a1e201690b
saraivais <isadorasaraiva95@gmail.com>
2022-07-16 21:16:07 -0300

    feat [app]: imports components and sets up routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Projects from "./routes/Projects";
+import Contact from "./routes/Contact";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Socorro deus
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/projects" element={ <Projects /> } />
+      <Route path="/contact" element={ <Contact /> } />
+      <Route path="/" element={ <Home /> } />
+    </Routes>
   );
 }
 
