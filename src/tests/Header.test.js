d012f25330f133c978c054728f762525f04d6724
saraivais <isadorasaraiva95@gmail.com>
2022-07-17 15:32:08 -0300

    fix [header.test]: adds memoryRouter so Links can be rendered~

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,12 +1,16 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
-// import { MemoryRouter } from 'react-router-dom'
+import { MemoryRouter } from 'react-router-dom';
 import Header from '../components/Header';
 
 describe('Verifies the correct rendering of the Header component', () => {
   it('verifies the presence of the expected links in the Header component', () => {
-    const { debug } = render(<Header />);
+    const { debug } = render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>,
+    );
     debug();
 
     const headerNavigationLinks = screen.getAllByRole('link');
