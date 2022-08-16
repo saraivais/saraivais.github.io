d743e5190f206b403cd7a9bcc32644d359c7de9c
saraivais <isadorasaraiva95@gmail.com>
2022-08-16 19:56:45 -0300

    feat [Footer: creates footer component~

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      Este Portfólio foi desenvolvido com React, CSS e amor.
+    </footer>
+  );
+}
+
+export default Footer;
