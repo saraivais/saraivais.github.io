7b93f10ade88c915aed3f486ea1b0f5fab86b03e
saraivais <isadorasaraiva95@gmail.com>
2022-08-16 14:55:14 -0300

    feat [Projects]: implements temporary projects page~

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
+import UnderCostruction from '../components/UnderConstruction';
+import '../style/Projects.css';
 
 function Projects() {
   return (
-    <div>
+    <div className="projects">
       <Header />
+      <UnderCostruction />
       Projects screen
     </div>
   );
