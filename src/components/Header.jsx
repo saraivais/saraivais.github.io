666a5c207e461acf27db7e5c73010ea609b8c342
saraivais <isadorasaraiva95@gmail.com>
2022-08-16 14:40:16 -0300

    feat [Header]: adds icons & classes to header~

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,37 @@
 import { Link } from 'react-router-dom';
+import home from '../icons/home.svg';
+import code from '../icons/code.svg';
+import email from '../icons/email.svg';
+import '../style/Header.css';
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/contact">Contact Me</Link>
+    <header className="header-container">
+
+      <Link to="/">
+        <img
+          src={home}
+          alt="home-icon"
+          className="header-icons"
+        />
+      </Link>
+
+      <Link to="/projects">
+        <img
+          src={code}
+          alt="code-icon"
+          className="header-icons"
+        />
+      </Link>
+
+      <Link to="/contact">
+        <img
+          src={email}
+          alt="contact-icon"
+          className="header-icons"
+        />
+      </Link>
+
     </header>
   );
 }
