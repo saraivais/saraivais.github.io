d967b4b9617dbadb9c0c9501917a5a4defe91eac
saraivais <isadorasaraiva95@gmail.com>
2022-08-16 14:43:02 -0300

    feat [Home]: implements home page~

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import Header from '../components/Header';
+import '../style/Home.css';
 
 function Home() {
   return (
-    <div>
+    <div className="home">
       <Header />
-      Home screen
+      <section className="introduction-section">
+        <div className="introduction-texts">
+          <h1 className="title">Meu nome é Isadora Saraiva</h1>
+          <h3 className="subtitle">Gosto de consertar coisas e resolver problemas</h3>
+          <p className="regular-text">
+            Vejo na tecnologia a possibilidade de evoluir como pessoa e profissional.
+          </p>
+          <p className="regular-text">
+            Estou estudando para me tornar uma Desenvolvedora Full Stack.
+            Em transição de carreira proveniente da Veterinária,
+            considero como meus pontos fortes organização, proatividade e dedicação.
+          </p>
+          <p className="regular-text">Nas horas vagas, pode me encontrar lendo, jogando ou cozinhando.</p>
+        </div>
+        <img
+          className="introduction-pic"
+          src="https://picsum.photos/200"
+          alt="profile-pic"
+        />
+      </section>
+      <section>IDK WHAT TO SAY HERE</section>
     </div>
   );
 }
