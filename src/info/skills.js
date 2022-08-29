a0158d3fa076427fbbe9d639657d9ff684a53a6e
saraivais <isadorasaraiva95@gmail.com>
2022-08-29 12:00:28 -0300

    feat [info]: adds array w skills while api is not ready

---
+++ src/info/skills.js
@@ -0,0 +1,155 @@
+import git from '../icons/git.svg';
+import github from '../icons/github.svg';
+import bash from '../icons/gnubash.svg';
+import html from '../icons/html5.svg';
+import css from '../icons/css3.svg';
+import js from '../icons/javascript.svg';
+import jest from '../icons/jest.svg';
+import react from '../icons/react.svg';
+import redux from '../icons/redux.svg';
+import rtl from '../icons/testinglibrary.svg';
+
+const skills = [
+  {
+    image: bash,
+    name: 'Bash',
+    description: 'Bourne Again Shell, é um shell Unix e um interpretador de linguagem de comando.',
+  },
+  {
+    image: git,
+    name: 'Git',
+    description: 'O git é um sistema open-source de controle de versão.',
+  },
+  {
+    image: github,
+    name: 'Github',
+    description: 'GitHub é um serviço de hospedagem para desenvolvimento de software e controle de versão usando Git.',
+  },
+  {
+    image: html,
+    name: 'HTML',
+    description: 'HTML (HyperText Markup Language) é uma linguagem de marcação, faz a estruturação de páginas web.',
+  },
+  {
+    image: css,
+    name: 'CSS',
+    description: 'CSS (Cascading Style Sheets) auxilia na personalização visual de páginas.',
+  },
+  {
+    image: js,
+    name: 'JavaScript',
+    description: 'JavaScript é uma linguagem de programação interpretada, orientada a objetos e baseada em protótipos.',
+  },
+  {
+    image: jest,
+    name: 'Jest',
+    description: 'É um framework de testes JavaScript desenvolvido pelo Facebook com base no Jasmine.',
+  },
+  {
+    image: react,
+    name: 'React',
+    description: 'É uma biblioteca JavaScript para construção de interfaces de usuário com base em componentes UI.',
+  },
+  {
+    image: redux,
+    name: 'Redux',
+    description: 'É uma biblioteca JavaScript para gerenciamento e centralização de estado de uma aplicação.',
+  },
+  {
+    image: rtl,
+    name: 'React Testing Library',
+    description: 'React Testing Library é um conjunto de utilitários que permitem testar componentes React.',
+  },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+  // {
+  //   image,
+  //   name,
+  //   description,
+  // },
+];
+
+export default skills;
