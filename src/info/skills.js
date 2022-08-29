3be90016978553cbd1b52534b4cc06db85326dad
saraivais <isadorasaraiva95@gmail.com>
2022-08-29 12:15:36 -0300

    feat [skills]: adds new info to skill array

--- src/info/skills.js
+++ src/info/skills.js
@@ -8,6 +8,18 @@ import jest from '../icons/jest.svg';
 import react from '../icons/react.svg';
 import redux from '../icons/redux.svg';
 import rtl from '../icons/testinglibrary.svg';
+import figma from '../icons/figma.svg';
+import trello from '../icons/trello.svg';
+import docker from '../icons/docker.svg';
+import mysql from '../icons/mysql.svg';
+import node from '../icons/nodedotjs.svg';
+import express from '../icons/express.svg';
+import mocha from '../icons/mocha.svg';
+import chai from '../icons/chai.svg';
+import vscode from '../icons/visualstudiocode.svg';
+import atom from '../icons/atom.svg';
+import reactrouter from '../icons/reactrouter.svg';
+import sequelize from '../icons/sequelize.svg';
 
 const skills = [
   {
@@ -19,6 +31,16 @@ const skills = [
     image: git,
     name: 'Git',
     description: 'O git é um sistema open-source de controle de versão.',
+  },
+  {
+    image: vscode,
+    name: 'Visual Studio Code',
+    description: 'O VSCode é um editor de código-fonte feito pela Microsoft.',
+  },
+  {
+    image: atom,
+    name: 'Atom',
+    description: 'Atom é um editor de texto e código-fonte open-source com suporte para Git.',
   },
   {
     image: github,
@@ -51,6 +73,11 @@ const skills = [
     description: 'É uma biblioteca JavaScript para construção de interfaces de usuário com base em componentes UI.',
   },
   {
+    image: reactrouter,
+    name: 'React Router',
+    description: 'O React Router é uma biblioteca de roteamento leve e completa para a biblioteca React JavaScript.',
+  },
+  {
     image: redux,
     name: 'Redux',
     description: 'É uma biblioteca JavaScript para gerenciamento e centralização de estado de uma aplicação.',
@@ -60,56 +87,51 @@ const skills = [
     name: 'React Testing Library',
     description: 'React Testing Library é um conjunto de utilitários que permitem testar componentes React.',
   },
-  // {
-  //   image,
-  //   name,
-  //   description,
-  // },
-  // {
-  //   image,
-  //   name,
-  //   description,
-  // },
-  // {
-  //   image,
-  //   name,
-  //   description,
-  // },
-  // {
-  //   image,
-  //   name,
-  //   description,
-  // },
-  // {
-  //   image,
-  //   name,
-  //   description,
-  // },
-  // {
-  //   image,
-  //   name,
-  //   description,
-  // },
-  // {
-  //   image,
-  //   name,
-  //   description,
-  // },
-  // {
-  //   image,
-  //   name,
-  //   description,
-  // },
-  // {
-  //   image,
-  //   name,
-  //   description,
-  // },
-  // {
-  //   image,
-  //   name,
-  //   description,
-  // },
+  {
+    image: figma,
+    name: 'Figma',
+    description: 'O Figma é uma ferramenta colaborativa de design de interface baseada em navegador.',
+  },
+  {
+    image: trello,
+    name: 'Trello',
+    description: 'O Trello é um aplicativo de criação de listas baseado na Web, no estilo Kanban.',
+  },
+  {
+    image: docker,
+    name: 'Docker',
+    description: 'Docker é um conjunto de produtos de plataforma como serviço (PaaS) que usam virtualização de nível de sistema operacional para entregar software em pacotes chamados contêineres.',
+  },
+  {
+    image: mysql,
+    name: 'MySQL',
+    description: 'MySQL é um sistema de gerenciamento de banco de dados relacional de código aberto.',
+  },
+  {
+    image: node,
+    name: 'Node.js',
+    description: 'O Node.js é um ambiente de tempo de execução JavaScript back-end de código aberto e multiplataforma.',
+  },
+  {
+    image: express,
+    name: 'Express.js',
+    description: 'Express.js, ou simplesmente Express, é uma estrutura de aplicativo da Web de back-end para Node.js, lançada como software livre e de código aberto.',
+  },
+  {
+    image: mocha,
+    name: 'Mocha',
+    description: 'Mocha é uma estrutura de teste JavaScript para programas Node.js, com suporte a navegadores, testes assíncronos, relatórios de cobertura de testes e uso de qualquer biblioteca de asserção.',
+  },
+  {
+    image: chai,
+    name: 'Chai',
+    description: 'Chai é uma biblioteca de asserções BDD/TDD para node e navegador que pode ser emparelhada com qualquer framework de teste javascript.',
+  },
+  {
+    image: sequelize,
+    name: 'Sequelize',
+    description: 'Sequelize é um ORM TypeScript e Node.js moderno para Postgres, MySQL, MariaDB, SQLite e SQL Server e muito mais.',
+  },
   // {
   //   image,
   //   name,
